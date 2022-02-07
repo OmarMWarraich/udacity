d5845c6abf6d18fe18482e2e8d3a9d48f2984bd9
OmarMWarraich <earthling986@gmail.com>
2022-02-08 03:28:35 +0500

    Add Contacts to the Server

--- react/reactnd-contacts-app/src/App.js
+++ react/reactnd-contacts-app/src/App.js
@@ -25,6 +25,14 @@ class App extends Component{
     }))
     ContactsAPI.remove(contact)
   }
+  createContact = (contact) => {
+    ContactsAPI.create(contact)
+      .then((contact) => {
+        this.setState((currentState) => ({
+          contacts: currentState.contacts.concat([contact])
+        }))
+      })
+  }
   render() {
     return (
       <div>
@@ -36,7 +44,14 @@ class App extends Component{
             onDeleteContact={this.removeContact}
           />}
         />
-        <Route path='/create' element={<CreateContact/>} />
+          <Route path='/create' render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.CreateContact(contact)
+              history.push('/')
+            }}
+          />
+        )} />
         </Routes>
         
       </div>
